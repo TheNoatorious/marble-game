14f930338d559e66ba0f3143e7a3e83ce1bd8e17
N.Z. Timisela <noatimisela@gmail.com>
2024-09-30 23:44:18 +0200

    Optimize: BlockStart with JSDoc

--- src/components/molecules/BlockStart/BlockStart.tsx
+++ src/components/molecules/BlockStart/BlockStart.tsx
@@ -1,9 +1,32 @@
-const BlockStart = ({ position = [0, 0, 0] }: any) => {
+import * as THREE from "three";
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+/**
+ * BlockStart Component
+ *
+ * This component creates a basic platform using a box geometry, and applies a standard material with a color.
+ * The block is placed at a configurable position and can be used as a starting point
+ *
+ * @param {Array<number>} [position=[0, 0, 0]] - The position of the block in 3D space.
+ *        Defaults to the origin if not provided.
+ *        [x, y, z] where:
+ *        - `x` is the horizontal position,
+ *        - `y` is the vertical position,
+ *        - `z` is the depth position.
+ *
+ * @returns {React.JSX.Element} A ThreeJS group containing a single mesh representing a block.
+ */
+const BlockStart = ({ position = [0, 0, 0] }: any): React.JSX.Element => {
     return (
         <>
             <group position={position}>
-                <mesh position={[0, -0.1, 0]} receiveShadow>
-                    <boxGeometry args={[4, 0.2, 4]} />
+                <mesh
+                    geometry={boxGeometry}
+                    position={[0, -0.1, 0]}
+                    receiveShadow
+                    scale={[4, 0.2, 4]}
+                >
                     <meshStandardMaterial color="limegreen" />
                 </mesh>
             </group>
