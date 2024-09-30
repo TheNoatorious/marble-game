f29788cbb65c3250490f0c1ce29f8ac67fb8d50f
N.Z. Timisela <noatimisela@gmail.com>
2024-09-30 22:57:23 +0200

    Add: BlockStart components and remove other meshes

---
+++ src/components/molecules/BlockStart/BlockStart.tsx
@@ -0,0 +1,11 @@
+const BlockStart = () => {
+    return (
+        <>
+            <mesh position={[0, -0.1, 0]} receiveShadow>
+                <boxGeometry args={[4, 0.2, 4]} />
+                <meshStandardMaterial color="limegreen" />
+            </mesh>
+        </>
+    );
+};
+export default BlockStart;
--- src/components/molecules/Level/Level.tsx
+++ src/components/molecules/Level/Level.tsx
@@ -1,25 +1,9 @@
+import BlockStart from "../BlockStart/BlockStart";
+
 const Level = () => {
     return (
         <>
-            <mesh castShadow position-x={-2}>
-                <sphereGeometry />
-                <meshStandardMaterial color="orange" />
-            </mesh>
-
-            <mesh castShadow position-x={2} scale={1.5}>
-                <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
-
-            <mesh
-                receiveShadow
-                position-y={-1}
-                rotation-x={-Math.PI * 0.5}
-                scale={10}
-            >
-                <planeGeometry />
-                <meshStandardMaterial color="greenyellow" />
-            </mesh>
+            <BlockStart />
         </>
     );
 };
