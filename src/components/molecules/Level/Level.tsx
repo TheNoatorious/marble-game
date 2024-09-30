ab95d8da4c978e18c9a7061f7a477c09c9d4b896
N.Z. Timisela <noatimisela@gmail.com>
2024-10-01 00:06:43 +0200

    Add: JSDoc to Level component

--- src/components/molecules/Level/Level.tsx
+++ src/components/molecules/Level/Level.tsx
@@ -1,6 +1,15 @@
 import BlockStart from "../BlockStart/BlockStart";
 
-const Level = () => {
+/**
+ * Level component
+ *
+ * This component serves as a building block for larger
+ * environments or levels.
+ *
+ * @returns {React.JSX.Element} A 3D level consisting of various components
+ */
+
+const Level = (): React.JSX.Element => {
     return (
         <>
             <BlockStart position={[0, 0, 0]} />
