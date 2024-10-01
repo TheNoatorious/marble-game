ede0aa9af7917ee3c8d66c1d6a7b68ae4e662f36
N.Z. Timisela <noatimisela@gmail.com>
2024-10-01 17:44:11 +0200

    Remove: Unused material

--- src/components/atoms/BlockStart/BlockStart.tsx
+++ src/components/atoms/BlockStart/BlockStart.tsx
@@ -2,8 +2,7 @@ import * as THREE from "three";
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 
-const floor1Material = new THREE.MeshStandardMaterial({ color: "limegreen" });
-const floor2Material = new THREE.MeshStandardMaterial({ color: "greenyellow" });
+const floor1Material = new THREE.MeshStandardMaterial({ color: 0xffffff });
 const obstacleMaterial = new THREE.MeshStandardMaterial({ color: "orangered" });
 const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
 
