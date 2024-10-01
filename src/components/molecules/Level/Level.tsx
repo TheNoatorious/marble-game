2c3023348d05aa85a7537de7ae41f1dfb863bd0a
N.Z. Timisela <noatimisela@gmail.com>
2024-10-02 01:13:37 +0200

    Remove: Static blocks

--- src/components/molecules/Level/Level.tsx
+++ src/components/molecules/Level/Level.tsx
@@ -16,11 +16,7 @@ import BlockStart from "../BlockStart/BlockStart";
 const Level = (): React.JSX.Element => {
     return (
         <>
-            <BlockStart position={[0, 0, 16]} />
-            <BlockSpinner position={[0, 0, 12]} />
-            <BlockLimbo position={[0, 0, 8]} />
-            <BlockAxe position={[0, 0, 4]} />
-            <BlockEnd position={[0, 0, 0]} />
+            <BlockStart position={[0, 0, 0]} />
         </>
     );
 };
