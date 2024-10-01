b5df41899c753d89115f75475c34982700e2ab94
N.Z. Timisela <noatimisela@gmail.com>
2024-10-02 00:54:43 +0200

    Update: Remove CSS background and add Sky environment with Clouds

--- src/components/organisms/Experience/Experience.tsx
+++ src/components/organisms/Experience/Experience.tsx
@@ -1,5 +1,12 @@
+import * as THREE from "three";
 import { Physics } from "@react-three/rapier";
-import { OrbitControls } from "@react-three/drei";
+import {
+    Cloud,
+    Clouds,
+    Environment,
+    OrbitControls,
+    Sky,
+} from "@react-three/drei";
 import Lights from "../../atoms/Lights/Lights";
 import Level from "../../molecules/Level/Level";
 
@@ -11,6 +18,14 @@ const Experience = () => {
                 <Lights />
                 <Level />
             </Physics>
+            <Clouds material={THREE.MeshBasicMaterial} position={[0, 4, -10]}>
+                <Cloud segments={10} bounds={[10, 2, 2]} volume={10} />
+
+                <Cloud seed={1} scale={2} volume={5} color="white" fade={100} />
+            </Clouds>
+
+            <Environment preset="dawn" />
+            <Sky />
         </>
     );
 };
