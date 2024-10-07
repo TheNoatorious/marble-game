c1e7817b2d1590e573c65fefad14e3e620729fbc
N.Z. Timisela <noatimisela@gmail.com>
2024-10-07 20:19:14 +0200

    Add: CuboidCollider import

--- src/components/atoms/Bounds/Bounds.tsx
+++ src/components/atoms/Bounds/Bounds.tsx
@@ -1,5 +1,5 @@
 import { MeshReflectorMaterial, Reflector } from "@react-three/drei";
-import { RigidBody } from "@react-three/rapier";
+import { CuboidCollider, RigidBody } from "@react-three/rapier";
 import * as THREE from "three";
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
@@ -33,6 +33,12 @@ const Bounds = ({ length = 1 }: any) => {
                     material={wallMaterial}
                     scale={[4, 1.5, 0.3]}
                     receiveShadow
+                />{" "}
+                <CuboidCollider
+                    args={[2, 0.1, 2 * length]}
+                    position={[0, -0.1, -(length * 2) + 2]}
+                    restitution={0.2}
+                    friction={1}
                 />
             </RigidBody>
         </>
