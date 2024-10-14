47e4285dc041a14e2aecdb39f7fad4b7edcc04cf
N.Z. Timisela <noatimisela@gmail.com>
2024-10-14 23:39:24 +0200

    Add: useFrame and listen to keyboard inputs

--- src/components/atoms/Player/Player.tsx
+++ src/components/atoms/Player/Player.tsx
@@ -1,6 +1,15 @@
+import { useRef } from "react";
+import { useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
 
 const Player = () => {
+    const [subscribeKeys, getKeys] = useKeyboardControls();
+
+    useFrame(() => {
+        const { forward, backward, leftward, rightward } = getKeys();
+    });
+
     return (
         <RigidBody
             canSleep={false}
