2b08223c7fa9441e283fa0b1e41e36216f82f4ff
N.Z. Timisela <noatimisela@gmail.com>
2024-12-27 23:30:26 +0100

    Add: Restart button

--- src/components/organisms/PlayerInterface/PlayerInterface.tsx
+++ src/components/organisms/PlayerInterface/PlayerInterface.tsx
@@ -4,6 +4,9 @@ const playerInterface = () => {
     return (
         <div className="player-interface">
             <div className="player-interface__time">0.00</div>
+            <div className="player-interface__btn player-interface__btn--restart">
+                Restart
+            </div>
         </div>
     );
 };
