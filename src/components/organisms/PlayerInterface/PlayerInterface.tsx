a0ed0d2c760ea36fb03ac0a4dae69ade9f8249f6
N.Z. Timisela <noatimisela@gmail.com>
2024-12-28 15:21:31 +0100

    Add: Player controls interface

--- src/components/organisms/PlayerInterface/PlayerInterface.tsx
+++ src/components/organisms/PlayerInterface/PlayerInterface.tsx
@@ -1,11 +1,35 @@
-import "./playerInterface.css";
+import "./PlayerInterface.css";
+import { useKeyboardControls } from "@react-three/drei";
 
 const playerInterface = () => {
+    const forward = useKeyboardControls((state) => state.forward);
+    const backward = useKeyboardControls((state) => state.backward);
+    const leftward = useKeyboardControls((state) => state.leftward);
+    const rightward = useKeyboardControls((state) => state.rightward);
+    const jump = useKeyboardControls((state) => state.jump);
+
+    console.log(forward, backward, leftward, rightward, jump);
+
     return (
         <div className="player-interface">
             <div className="player-interface__time">0.00</div>
             <div className="player-interface__btn player-interface__btn--restart">
                 Restart
+            </div>
+
+            {/* Controls */}
+            <div className="player-interface__controls">
+                <div className="player-interface__row">
+                    <div className="player-interface__key"></div>
+                </div>
+                <div className="player-interface__row">
+                    <div className="player-interface__key"></div>
+                    <div className="player-interface__key"></div>
+                    <div className="player-interface__key"></div>
+                </div>
+                <div className="player-interface__row">
+                    <div className="player-interface__key player-interface__key--large"></div>
+                </div>
             </div>
         </div>
     );
