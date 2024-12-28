70ad9da5ef610941577951800bfc413461c0c2ae
N.Z. Timisela <noatimisela@gmail.com>
2024-12-28 15:25:54 +0100

    Fix: Interface component styling

--- src/components/organisms/PlayerInterface/PlayerInterface.tsx
+++ src/components/organisms/PlayerInterface/PlayerInterface.tsx
@@ -13,9 +13,7 @@ const playerInterface = () => {
     return (
         <div className="player-interface">
             <div className="player-interface__time">0.00</div>
-            <div className="player-interface__btn player-interface__btn--restart">
-                Restart
-            </div>
+            <div className="player-interface__restart">Restart</div>
 
             {/* Controls */}
             <div className="player-interface__controls">
