22a54030bfe7960a4e054ce9a78269b647d042b0
N.Z. Timisela <noatimisela@gmail.com>
2025-02-03 22:47:05 +0100

    Add: Subscribe functionality for store

--- src/stores/useGame.ts
+++ src/stores/useGame.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { subscribeWithSelector } from "zustand/middleware";
 
 // Type defining
 type GameState = {
@@ -10,26 +11,27 @@ type GameState = {
     end: () => void;
 };
 
-const useGame = create<GameState>((set) => ({
-    blocksCount: 3,
-    phase: "ready",
-    start: () =>
-        set((state) => ({
-            phase: state.phase === "ready" ? "playing" : state.phase, // Only change to "playing" if it's not already "ready"
-        })),
+const useGame = create<GameState>()(
+    subscribeWithSelector((set) => ({
+        blocksCount: 3,
+        phase: "ready",
+        start: () =>
+            set((state) => ({
+                phase: state.phase === "ready" ? "playing" : state.phase, // Only change to "playing" if it's not already "ready"
+            })),
 
-    restart: () =>
-        set((state) => ({
-            phase:
-                state.phase === "playing" || state.phase === "ended"
-                    ? "ready"
-                    : state.phase, // Only restart if in "playing" or "ended"
-        })),
+        restart: () =>
+            set((state) => ({
+                phase:
+                    state.phase === "playing" || state.phase === "ended"
+                        ? "ready"
+                        : state.phase, // Only restart if in "playing" or "ended"
+            })),
 
-    end: () =>
-        set((state) => ({
-            phase: state.phase === "playing" ? "ended" : state.phase, // Only end the game if it's "playing"
-        })),
-}));
-
+        end: () =>
+            set((state) => ({
+                phase: state.phase === "playing" ? "ended" : state.phase, // Only end the game if it's "playing"
+            })),
+    }))
+);
 export default useGame;
