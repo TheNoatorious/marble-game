174b3956cd8dd8d05733691ec54e24abbe8e5d7f
N.Z. Timisela <noatimisela@gmail.com>
2025-02-08 21:27:44 +0100

    Add: Store variables and implement game functions

--- src/stores/useGame.ts
+++ src/stores/useGame.ts
@@ -4,7 +4,10 @@ import { subscribeWithSelector } from "zustand/middleware";
 // Type defining
 type GameState = {
     blocksCount: number;
+    blocksSeed: number;
     phase: "ready" | "playing" | "ended";
+    startTime: number;
+    endTime: number;
 
     start: () => void;
     restart: () => void;
@@ -13,25 +16,35 @@ type GameState = {
 
 const useGame = create<GameState>()(
     subscribeWithSelector((set) => ({
-        blocksCount: 3,
+        blocksCount: 10,
+        blocksSeed: 0,
         phase: "ready",
-        start: () =>
-            set((state) => ({
-                phase: state.phase === "ready" ? "playing" : state.phase, // Only change to "playing" if it's not already "ready"
-            })),
+        startTime: 0,
+        endTime: 0,
 
-        restart: () =>
+        start: () => {
             set((state) => ({
-                phase:
-                    state.phase === "playing" || state.phase === "ended"
-                        ? "ready"
-                        : state.phase, // Only restart if in "playing" or "ended"
-            })),
+                phase: state.phase === "ready" ? "playing" : state.phase,
+                startTime:
+                    state.phase === "ready" ? Date.now() : state.startTime, // Set startTime only when phase changes to "playing"
+            }));
+        },
 
-        end: () =>
+        restart: () => {
+            set((state) => {
+                if (state.phase === "playing" || state.phase === "ended") {
+                    return { phase: "ready", blocksSeed: Math.random() };
+                }
+                return state; // Returns the current state if the condition is not met
+            });
+        },
+
+        end: () => {
             set((state) => ({
                 phase: state.phase === "playing" ? "ended" : state.phase, // Only end the game if it's "playing"
-            })),
+                endTime: state.phase === "playing" ? Date.now() : state.endTime, // Set endTime only when phase is "ended"
+            }));
+        },
     }))
 );
 export default useGame;
